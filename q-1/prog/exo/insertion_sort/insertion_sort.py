2160d572c72fece17e9afe28be210f8e8e9216e4
KoruJuriSan <mr.collejoulian@gmail.com>
2024-01-14 16:57:28 +0100

    edit: simplification in the correction of the insert_sort + correction condtion -> condition

--- q-1/prog/exo/insertion_sort/insertion_sort.py
+++ q-1/prog/exo/insertion_sort/insertion_sort.py
@@ -1,13 +1,11 @@
-def insertion_sort(list, condtion):
-    tempon = 0
-    transmutation = 0
-    for key in range(1, len(list)):
-        tempon = list[key]
-        transmutation = key - 1
-        while transmutation >= 0 and condtion(tempon, list[transmutation]):
-            list[transmutation + 1] = list[transmutation]
-            transmutation -= 1
-        list[transmutation + 1] = tempon
+def insert_sort(list, condition):
+    for i in range(1, len(list)):
+        cache = list[i]
+        j = i - 1
+        while j >= 0 and condition(cache, list[j]):
+            list[j + 1] = list[j]
+            j -= 1
+        list[j + 1] = cache
 
 
 def condition(element_1, element_2):
@@ -15,5 +13,5 @@ def condition(element_1, element_2):
 
 
 my_list = [9, 4, 3, 2, 6, 5]
-insertion_sort(my_list, condition)
+insert_sort(my_list, condition)
 print(my_list)
